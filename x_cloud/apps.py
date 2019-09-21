0c5ad83ba49a21470a27c4c1c7ff84aa915e54d4
Nick Hsu <nick.hsu@wish-computer.com>
2019-09-21 09:56:32 +0800

    first commit

---
+++ x_cloud/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class XCloudConfig(AppConfig):
+    name = 'x_cloud'
---
+++ x_cloud/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Login(request):
+	return render(request, 'Login.html', locals())
